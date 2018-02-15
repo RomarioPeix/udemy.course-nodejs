b5befce19dc7056267ed40c0f093f62c992ed731
RomarioPeix <jrdapgro@gmail.com>
2018-02-15 17:13:26 -0200

    body parser and initial form

--- app/routes/admin.routes.js
+++ app/routes/admin.routes.js
@@ -2,5 +2,11 @@
 module.exports = function(app) {
   app.get('/formulario-inclusao-noticia', function(req, res){
     res.render('../views/admin/form_add_noticia');
+    
+    //saving the new noticia
+    app.post('/noticias/salvar', function(req, res){
+      var noticias = req.body;
+      res.send(noticias);
+    });
   });
 }
